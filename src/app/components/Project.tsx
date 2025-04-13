271eb7b543d8888cc3b7142d071c2c49b8857f07
godless-developer <bbtulga98@gmail.com>
2025-04-13 20:14:18 +0800

    project done2

--- src/app/components/Project.tsx
+++ src/app/components/Project.tsx
@@ -14,6 +14,33 @@ import {
 import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
 
+const UPLOAD_PRESET = process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET!;
+const CLOUD_NAME = process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME;
+
+const uploadImage = async (file: File | null) => {
+  if (!file) {
+    return null;
+  }
+
+  const formData = new FormData();
+  formData.append("file", file);
+  formData.append("upload_preset", UPLOAD_PRESET);
+
+  try {
+    const response = await fetch(
+      `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/image/upload`,
+      {
+        method: "POST",
+        body: formData,
+      }
+    );
+    const result = await response.json();
+    return result.secure_url;
+  } catch (error: unknown) {
+    return { error: "failed to upload image" };
+  }
+};
+
 export default function ProjectsSection() {
   const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
   const [isHovered, setIsHovered] = useState(false);
